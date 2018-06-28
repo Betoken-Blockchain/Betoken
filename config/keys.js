d640f4747e632c0672bdbbd6f6c34c313d042ba6
benjaminsinaiko <ben.jamin@outlook.com>
2018-06-28 17:29:50 -0500

    Add config and api folders, update server to connect with mongodb

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds121321.mlab.com:21321/betoken`
+};
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 require('dotenv').config();
+console.log(process.env.SECRET);
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 

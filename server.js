f2ec57e067148d249a27da316236deadd5f7839f
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-05 18:37:34 -0500

    Add user registration route, initial  user login route

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
 const app = express();
 
 // Body parser middleware
@@ -22,6 +25,10 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
 

19ade4b8ec360a588f25fc5c2984909cc772d69e
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-06 01:08:14 -0500

    Add jwt authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+
 
 // Load User model
 const User = require('../../models/User');
@@ -58,12 +62,36 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({ msg: 'Success' });
+            // User Matched
+
+            const payload = { id: user.id, name: user.name }; // Create JWT payload
+
+            // Sign Token
+            jwt.sign(
+              payload,
+              keys.secretOrKey, { expiresIn: 86400 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
           } else {
             return res.status(400).json({ password: 'Password incorrect' });
           }
         });
     });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+  // res.json(req.user);
+  res.json({
+    id: req.user.id,
+    name: req.user.name
+  });
 });
 
 module.exports = router;

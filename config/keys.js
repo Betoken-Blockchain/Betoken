0745a50b42aab992292707d4a1d9ef2b14f61f69
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-05 19:47:40 -0500

    Add passport.js in config

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds121321.mlab.com:21321/betoken`
+  mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ds121321.mlab.com:21321/betoken`,
+  secretOrKey: process.env.PASS_SECRET
 };

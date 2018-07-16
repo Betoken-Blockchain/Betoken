62657930c2785883fb7dfa29efb568479391a9a5
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-16 13:17:31 -0500

    Add basic Profile model

---
+++ models/Profile.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  social: {
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('Profile', ProfileSchema);

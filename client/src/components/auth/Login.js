be2a2d901969f4f107e11ac0dc908f32bd040544
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-23 16:04:21 -0500

    Create registration and login forms, for styling

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,73 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Auth.css';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log('user', user);
+  }
+
   render() {
     return (
       <div>
         <h1>Login</h1>
+
+        <div className="col-md-6">
+          <div id="logbox">
+            <form onSubmit={this.onSubmit}>
+              <h1>account login</h1>
+              <input
+                className="input pass"
+                name="email"
+                type="email"
+                placeholder="enter your email"
+                value={this.state.email}
+                onChange={this.onChange}
+                autoFocus
+              />
+              <input
+                className="input pass"
+                name="password"
+                type="password"
+                placeholder="enter your password"
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+              <input
+                className="inputButton"
+                type="submit"
+                value="Sign me in!"
+              />
+              <div className="text-center">
+                <Link to="/register">create an account</Link> -{' '}
+                <Link to="/">forgot password</Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,90 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import './Auth.css';
 class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    console.log('newUser', newUser);
+  }
+
   render() {
     return (
       <div>
         <h1>Register</h1>
+        <div className="col-md-6">
+          <div id="logbox">
+            <form onSubmit={this.onSubmit}>
+              <h1>create an account</h1>
+              <input
+                className="input pass"
+                name="name"
+                type="text"
+                placeholder="What's your name?"
+                value={this.state.name}
+                onChange={this.onChange}
+                autoFocus
+              />
+              <input
+                className="input pass"
+                name="email"
+                type="email"
+                placeholder="Email address"
+                value={this.state.email}
+                onChange={this.onChange}
+              />
+              <input
+                className="input pass"
+                name="password"
+                type="password"
+                placeholder="Choose a password"
+                value={this.state.password}
+                onChange={this.onChange}
+              />
+              <input
+                className="input pass"
+                name="password2"
+                type="password"
+                placeholder="Confirm password"
+                value={this.state.password2}
+                onChange={this.onChange}
+              />
+              <input
+                className="inputButton"
+                type="submit"
+                value="Sign me up!"
+              />
+              <div className="text-center">
+                already have an account? <Link to="/login">login</Link>
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }

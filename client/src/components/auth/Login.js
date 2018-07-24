c6bd2818732ee618c0dc6f6c5933599eac8dfb64
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-24 15:29:07 -0500

    Logout and conditional navbar links

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+import { loginUser } from '../../actions/authActions';
+
 import './Auth.css';
 
 class Login extends Component {
@@ -19,18 +24,36 @@ class Login extends Component {
     this.setState({ [e.target.name]: e.target.value });
   }
 
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const userData = {
       email: this.state.email,
       password: this.state.password
     };
 
-    console.log('user', user);
+    this.props.loginUser(userData);
   }
 
   render() {
+    const { errors } = this.state;
+
     return (
       <div>
         <h1>Login</h1>
@@ -40,7 +63,9 @@ class Login extends Component {
             <form onSubmit={this.onSubmit}>
               <h1>account login</h1>
               <input
-                className="input pass form-control"
+                className={classnames('input pass form-control', {
+                  'is-invalid': errors.email
+                })}
                 name="email"
                 type="email"
                 placeholder="enter your email"
@@ -48,14 +73,22 @@ class Login extends Component {
                 onChange={this.onChange}
                 autoFocus
               />
+              {errors.email && (
+                <div className="invalid-feedback">{errors.email}</div>
+              )}
               <input
-                className="input pass form-control"
+                className={classnames('input pass form-control', {
+                  'is-invalid': errors.password
+                })}
                 name="password"
                 type="password"
                 placeholder="enter your password"
                 value={this.state.password}
                 onChange={this.onChange}
               />
+              {errors.password && (
+                <div className="invalid-feedback">{errors.password}</div>
+              )}
               <input
                 className="inputButton"
                 type="submit"
@@ -73,4 +106,18 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  loginUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);

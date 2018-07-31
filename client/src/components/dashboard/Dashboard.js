28a40e868e6800f7fe8b660a1c8ef9a94282c958
benjaminsinaiko <ben.jamin@outlook.com>
2018-07-30 20:50:36 -0500

    Update #logbox css, dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -26,7 +26,7 @@ class Dashboard extends Component {
         // User is logged in but has no prifle
         dashboardContent = (
           <div>
-            <p className="lead text-muted">Welcom {user.name}</p>
+            <p className="lead text-muted">Welcome {user.name}</p>
             <p>You have not yet set up a profile, please add some info</p>
             <Link to="/create-profile" className="btn btn-lg btn-info">
               Create Profile

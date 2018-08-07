b9c341af46a77eeda88c74502f44406b03fde700
benjaminsinaiko <ben.jamin@outlook.com>
2018-08-07 16:28:39 -0500

    Update eventlist format, with placeholders

--- client/src/components/events/EventsList.js
+++ client/src/components/events/EventsList.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react';
 import Moment from 'react-moment';
 
+import './Events.css';
+
 class EventsList extends Component {
   render() {
     const { events } = this.props;
 
     const eventsItem = events.map(event => (
-      <div key={events.id} class="card card-body mb-2">
-        <div class="row">
-          <div class="col-md-6">
-            <span class="badge badge-info mr-1">
-              {event.homeTeam.City} {event.homeTeam.Name}
-            </span>
-            <span class="badge badge-secondary mr-1">@ {event.location}</span>
-            <span class="badge badge-success">
-              {event.awayTeam.City} {event.awayTeam.Name}
-            </span>
+      <div key={events.id} className="card card-body mb-2">
+        <div className="games-schedule-items">
+          <div className="row games-team">
+            <div className="col-md-5">
+              <img src="http://placehold.it/115x67" alt="Away Team" />
+              <span>
+                {event.awayTeam.City} {event.awayTeam.Name}
+              </span>
+            </div>
+            <div className="col-md-2">
+              <h4 className="img-circle">VS</h4>
+            </div>
+            <div className="col-md-5">
+              <img src="http://placehold.it/115x67" alt="Home Team" />
+              <span>
+                {event.homeTeam.City} {event.homeTeam.Name}
+              </span>
+            </div>
           </div>
-          <div class="col-md-6">
-            <p>
-              <Moment format="MMMM do, YYYY">{event.date}</Moment>
-            </p>
-            <p>{event.time}</p>
+          <div className="row games-info">
+            <div className="col-md-12">
+              <p>
+                <span className="glyphicon glyphicon-play-circle" />
+                <Moment format="MMM do, YYYY">{event.date}</Moment> (<small>
+                  {event.time}
+                </small>)
+              </p>
+              <p className="games-dash" />
+              <p>
+                <small>
+                  {event.location}, {event.homeTeam.City}
+                </small>
+              </p>
+            </div>
           </div>
         </div>
       </div>

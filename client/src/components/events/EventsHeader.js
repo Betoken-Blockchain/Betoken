48199837c19f3a25743d24c22aef223593f54521
benjaminsinaiko <ben.jamin@outlook.com>
2018-08-15 15:38:50 -0500

    Add mlb logo for games feed

--- client/src/components/events/EventsHeader.js
+++ client/src/components/events/EventsHeader.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
-
+import MLBLogo from '../../img/mlb/MLB-Logo.png';
 class EventsHeader extends Component {
   render() {
     const { sport } = this.props;
 
+    let mlbHeader = (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-info text-white mb3">
+            <div className="text-center">
+              <img
+                src={MLBLogo}
+                style={{ width: '25%', height: 'augo' }}
+                alt="mlb logo"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
     return (
       <div className="row">
         <div className="col-md-12">
-          <div className="card card-body bg-info text-white mb-3">
-            <div className="row" />
-            <div className="text-center">
-              <h1 className="display-4 text-center">{sport}</h1>
-            </div>
+          <div className="text-white mb-3">
+            <div className="text-center">{mlbHeader}</div>
           </div>
         </div>
       </div>
